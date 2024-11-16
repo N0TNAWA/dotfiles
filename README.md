549651375532d5ba61e40af8775103f0b2bcec9c
N0TNAWA <btorpsamuel@gmail.com>
2024-11-16 16:03:52 +0100

    added nvidia installation

--- README.md
+++ README.md
@@ -30,6 +30,53 @@ cd paru
 makepkg -si
 ```
 
-## Run the install script
+## Clone my dotfiles
 
-### If everything installed correctly you can now run the install script
+### Clone and install
+IMPORTANT: If you have a nvidia graphics card you will be prompted to install the nvidia drivers. Download those and follow the nvidia instructions further down when the installation is completed.
+```
+git clone https://github.com/N0TNAWA/dotfiles
+cd dotfiles
+chmod +x install.sh
+./install.sh
+```
+
+## Nvidia drivers
+Generate initramfs
+```
+sudo nano /etc/mkinitcpio.conf
+```
+Edit where you see 'MODULES=()' and add
+```
+MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)
+```
+Assuming that you are using GRUB boot you wanna make some changes in /etc/default/grub
+```
+sudo nano /etc/default/grub
+```
+And edit where you see 'GRUB_CMDLINE_LINUX_DEFAULT=""' and add
+```
+GRUB_CMDLINE_LINUX_DEFAULT="nvidia_drm.modeset=1"
+```
+Now we want to automatically update the drivers whenever we update the system. To do that we need to create a hook
+```
+sudo mkdir /etc/pacman.d/hooks
+sudo nano /etc/pacman.d/hooks/nvidia.hook
+```
+In this new file we wanna add these lines
+```
+[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+
+[Action]
+Depends=mkinitcpio
+When=PostTransaction
+Exec=/usr/bin/mkinitcpio -P
+```
+these lines just tells the drviers to update whenever /usr/bin/mkinitcpio -P is executed. Which happens every update.
+
+Now if you did everything right you should now only need to reboot.
--- install.sh
+++ install.sh
@@ -30,9 +30,35 @@ for app in "${flatpak_packages[@]}"; do
   flatpak install -y "$app"
 done
 
+echo "Do you want to install wine?(Y/n)"
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+  wine_packages=(wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader)
+  
+  sudo pacman -S "${wine_packages[@]}"
+
+  echo "Installed wine successfully."
+
+else
+  echo "Skipping wine installation."
+fi
+
+echo "Do you want to install nvidia drivers?(Y/n)"
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+  nvidia_packages=(nvidia-dkms nvidia-utils nvidia-settings lib32-nvidia-utils nvidia-libgl lib32-nvidia-libgl vulkan-icd-loader lib32-vulkan-icd-loader libvdpau mesa vdpauinfo libglvnd libva libva-utils lib32-mesa lib32-libvdpau lib32-libva-vdpau-driver lib32-libglvnd lib32-libva)
+
+  sudo pacman -S "${nvidia_packages[@]}"
+
+  echo "Installed nvidia successfully.(After the installation, follow the instructions on the github page to set up automatic updates.)"
+
+else
+  echo "Skipping nvidia installation."
+fi
+
 echo "Done installing flatpak packages!"
 echo "Installation stage complete."
 echo "Changing default shell."
+
+# change shell
 
 if ! chsh -s "$(which zsh)"; then
   echo "Failed to set zsh as the default shell"
@@ -42,13 +68,20 @@ fi
 
 echo "Using stow to set up symlinks from ~/dotfiles to ~/.config"
 
+# setting up symlinks between the dotfiles and the .config folder
+
 stow .
 
 echo "Setting up wallpaper and color scheme"
+
+# setting up color scheme
+
 wal -i "$HOME/Wallpapers/1.png"
 
 echo "Done setting up config symlinks."
 echo "Copying over assets."
+
+# setting up sddm
 
 usr=$(whoami)
 
@@ -59,6 +92,8 @@ sudo cp "$HOME/dotfiles/.config/assets/default.conf" "/usr/lib/sddm/sddm.conf.d/
 
 echo "Done copying assets."
 echo "Starting up systemctl services"
+
+# starting and enabling system services
 
 systemctl_services=(NetworkManager.service sshd.service sddm.service)
 
